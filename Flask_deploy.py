0e431192880ac8ac33c3ac7eb4898843fc3c130a
ubaidkhan08 <88443025+ubaidkhan08@users.noreply.github.com>
2022-10-30 22:06:31 +0530

    Update Flask_deploy.py

--- Flask_deploy.py
+++ Flask_deploy.py
@@ -13,25 +13,20 @@ def classify(sepal_length, sepal_width, petal_length, petal_width):
 
     
 def main():
-    st.title("Plant Species Prediction Model")
+    st.title("Major Project")
     html_temp = """
     <div style="background-color:teal ;padding:10px">
     <h2 style="color:white;text-align:center;">Iris Classification</h2>
     </div>
     """
     
-    st.markdown(html_temp, unsafe_allow_html=True)
-    sepal_length=st.slider('Select Sepal Length', 0.0, 10.0)
-    sepal_width=st.slider('Select Sepal Width', 0.0, 10.0)
-    petal_length=st.slider('Select Petal Length', 0.0, 10.0)
-    petal_width=st.slider('Select Petal Width', 0.0, 10.0)
-    inputs=np.array([[sepal_length, sepal_width, petal_length, petal_width]]).reshape(1, -1)
-   
+    
 
-    if st.button('Classify'):
-        output=classify(sepal_length, sepal_width, petal_length, petal_width)
-        st.success('The species of the plant is {}'.format(output[1:-1]))
-       
+    picture = st.camera_input("Take a picture")
+
+    if picture:
+        st.image(picture)
+   
      
 if __name__=='__main__':
     main()
